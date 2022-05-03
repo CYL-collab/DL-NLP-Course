496af2ce5be5d36633fc95feabe365cb4be0670c
CYL-collab <67359233+CYL-collab@users.noreply.github.com>
2022-05-03 17:52:56 +0800

    sklearn

--- LDA/LDA.py
+++
@@ -1,7 +0,0 @@
-import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-
-if __name__ == "__main__":
-    with open("LDA/doc.pkl", "rb") as f:
-        doc = pickle.load(f)
-    print(doc)
--- LDA/pre.py
+++ LDA/pre.py
@@ -1,6 +1,8 @@
 import jieba
 import os
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 def get_para(path,min_char):
     with open(path, "r", encoding="ANSI") as f:
@@ -21,6 +23,7 @@ def get_para(path,min_char):
                     paras.append(tmp_char)
                 break
     f.close()
+    paras = paras[::int(len(paras)/22)][0:22]
     return paras
   
 def word_seg(text):
@@ -28,17 +31,29 @@ def word_seg(text):
     seg_list = [" ".join(jieba.lcut(e, use_paddle=True, cut_all=False)) for e in text]
     return seg_list            
 
-def docs_gen(sourcepath, destination, char_len):
+def docs_gen(sourcepath, stpwrdpath, char_len):
     files = os.listdir(sourcepath)
-    docs = {}
+    with open(stpwrdpath, 'rb') as fp:
+        stopword = fp.read().decode('utf-8') 
+    stpwrdlst = stopword.splitlines()
+    seg_list = []
     for file in files:
         fullpath = sourcepath + '\\' + file
         filename = file[:-4]
-        docs[filename] = word_seg(get_para(fullpath, char_len))
-    with open(destination, "wb") as f:
-        pickle.dump(docs,f,pickle.HIGHEST_PROTOCOL)
-    f.close()
-    return docs
+        seg_list.extend(word_seg(get_para(fullpath, char_len)))
+    vec = CountVectorizer(token_pattern = r"(?u)\b\w+\b", ngram_range=(1,1), min_df = 1, stop_words=stpwrdlst)
+    cnt = vec.fit_transform(seg_list)
+    # print( 'vocabulary dic :\n\n',vec.vocabulary_)
+    return cnt
+
+# def gibbs():
+    
         
 if __name__ == "__main__":
-    docs_gen('LDA/text', 'LDA/doc.pkl', 500)
+    cnt = docs_gen('LDA/text', 'LDA/cn_stopwords.txt', 1000)
+    lda = LatentDirichletAllocation(n_components = 2, 
+                                    learning_offset = 50,
+                                    random_state = 0)
+    res = lda.fit_transform(cnt)
+    print(res)
+    
